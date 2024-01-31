2dee9f49cce07c5cdfb949a44738d08f024d48e8
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-01-31 07:53:19 +0300

    Аdd error handling

--- alice.py
+++ alice.py
@@ -14,16 +14,25 @@ def remove_duplicates(message):
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("Correct usage example: python alice.py path/to/file.txt")
+        sys.exit(1)
 
     file_path = sys.argv[1]
 
-    with open(file_path, 'r') as file:
-        data = file.read()
-        res = remove_duplicates(data)
-        print(res)
-        res_file = open("result.txt", "w+")
-        res_file.write(res)
-        res_file.close()
+    try:
+        with open(file_path, 'r') as file:
+            data = file.read()
+            res = remove_duplicates(data)
+            print(res)
+            res_file = open("result.txt", "w+")
+            res_file.write(res)
+            res_file.close()
+
+    except FileNotFoundError:
+        print(f"File not found: {file_path}. Choose correct path to file.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == '__main__':
