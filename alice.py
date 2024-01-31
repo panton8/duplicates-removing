36c704f122b696041280e89e6b97862c4b18adf7
Anton Padvalnikau <podvalnikov0@gmail.com>
2024-01-31 07:31:59 +0300

    Add function for duplicates removing

--- alice.py
+++ alice.py
@@ -1,5 +1,18 @@
+
+def remove_duplicates(message):
+    symbol_stack = []
+
+    for letter in message:
+        if symbol_stack and letter == symbol_stack[-1]:
+            symbol_stack.pop()
+        else:
+            symbol_stack.append(letter)
+
+    return ''.join(symbol_stack)
+
+
 def main():
-    pass
+    print(remove_duplicates('wwaldaadicffenn'))
 
 
 if __name__ == '__main__':
